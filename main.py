a12b267ce365e1a6c1f2ebfd5a73550f004a811f
huuvinhnhv <77050017+huuvinhnhv@users.noreply.github.com>
2023-05-01 14:43:29 +0700

    new code update
    modify code for new api

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import time
 
 app = FastAPI(title="Random Number Generator")
 
+
 # Define a CORS middleware
 origins = ["*"]
 app.add_middleware(
@@ -31,104 +32,72 @@ app.add_middleware(
 async def redirect_docs():
     return RedirectResponse("http://localhost:8000/docs")
 
-# Define a sample endpoint
-# @app.get("/token")
-# async def get_beaver_token():
-#     url = "https://raas.deta.dev/token"
-#     data = "username=demo&password=demo"
-#     headers = {"Content-Type": "application/x-www-form-urlencoded"}
-#     response = requests.post(url, data=data, headers=headers)
-#     beaver_token = response.json().get("access_token")
-#     print(f"Status code: {response.status_code}")
-#     return beaver_token
-
-# @app.get("/token")
-
-
-async def get_beaver_token():
-    url = "https://raas.deta.dev/token"
-    data = "username=demo&password=demo"
-    headers = {"Content-Type": "application/x-www-form-urlencoded"}
-    response = requests.post(url, data=data, headers=headers)
-    beaver_token = response.json().get("access_token")
-    print(f"Status code: {response.status_code}")
-    return beaver_token
-
-
-async def get_auth_header():
-    token = await get_beaver_token()
-    if token:
-        return {"Authorization": f"Bearer {token}"}
-    else:
-        raise HTTPException(status_code=401, detail="Unable to authenticate")
-
 
 @app.post("/getRequestId", tags=["GET Request Id"])
-async def get_request_id(auth_header: dict = Depends(get_auth_header)):
-    url = "https://raas.deta.dev/random"
-    request_body = {
-        "numWords": 1,
-        "callBackUrl": "https://raasdemo.deta.dev/fulfilment"
-    }
-    response = requests.post(url, json=request_body, headers=auth_header)
-    return response.json()
+async def get_request_id():
+    alchemy_url = "https://polygon-mumbai.g.alchemy.com/v2/gtP5XQRh8pmPyr4r5X5az9SLy-wauHKL"
+    w3 = Web3(Web3.HTTPProvider(alchemy_url))
+    contract_details = {}
+    with open('contract.json') as file:
+        contract_details = json.load(file)
+
+    my_address = "0xF3d50eF025A22A991B2972CBA2c5a0cd0a2952Bd"
+    private_key = "fc22142cc3ff6a682f291a0dd812068155f764f4932be95ea8b0afac009fcea2"
+
+    contract = w3.eth.contract(address=contract_details.get(
+        'address'), abi=contract_details.get('abi'))
+    nonce = w3.eth.get_transaction_count(my_address)
+
+    # Build tx for requestRandomWords
+    tx = contract.functions.requestRandomWords().build_transaction({
+        'chainId': 80001,
+        'gas': 2500000,
+        'maxFeePerGas': w3.to_wei('2', 'gwei'),
+        'maxPriorityFeePerGas': w3.to_wei('2', 'gwei'),
+        'nonce': nonce
+    })
+    # sign with private key
+    signed_tx = w3.eth.account.sign_transaction(tx, private_key)
+
+    # send signed_tx
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+
+    # wait for tx to be mined
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+    # check if tx is successful
+    if (tx_receipt.get('status')):
+        # process receipt - might fail due to micro timeout of 10s
+        requestId = str(contract.events.RequestSent(
+        ).process_receipt(tx_receipt)[0]['args']['requestId'])
+        return requestId
+    else:
+        # error
+        raise HTTPException(
+            status_code=500, detail="Request Not Processed, Kindly try again later")
 
 
 @app.get("/getRandomNumber/{requestId}", tags=["GET data by request Id"])
-async def read_data_by_request_id(requestId: int):
-    url = f"https://raas.deta.dev/draw/{requestId}"
-    count = 0
-    try:
-        while True:
-            response = requests.get(url)
-            time.sleep(5)
-            count += 5
-            print(count)
-            if response.status_code == 200:
-                break
-    except requests.exceptions.HTTPError as e:
-        return ({e.response.status_code})
-    except requests.exceptions.Timeout:
-        return ("Request timed out")
-    num = int(response.json()[0].get("randomWords")[1:-1])
-    address = '0x7a1bac17ccc5b313516c5e16fb24f7659aa5ebed'
-    apiKey = 'YV328YDS74FSM4YHYVFEXVJN9PBRJCWQUQ'
+def get_ramdom_number(requestId: int):
+    alchemy_url = "https://polygon-mumbai.g.alchemy.com/v2/gtP5XQRh8pmPyr4r5X5az9SLy-wauHKL"
+    w3 = Web3(Web3.HTTPProvider(alchemy_url))
+    contract_details = {}
+    with open('contract.json') as file:
+        contract_details = json.load(file)
 
-    blockTimeStamp = int(response.json()[0].get("timestamp"))
-    blockUrl = f'https://api-testnet.polygonscan.com/api?module=block&action=getblocknobytime&timestamp={blockTimeStamp}&closest=before&apikey={apiKey}'
-    blockNo = requests.get(blockUrl).json().get('result')
-    # print(blockNo)
+    my_address = "0xF3d50eF025A22A991B2972CBA2c5a0cd0a2952Bd"
+    private_key = "fc22142cc3ff6a682f291a0dd812068155f764f4932be95ea8b0afac009fcea2"
 
-    # get txHash of block
-    logUrl = f'https://api-testnet.polygonscan.com/api?module=logs&action=getLogs&fromBlock={blockNo}&toBlock={blockNo}&address={address}&apikey={apiKey}'
-
-    arr = requests.get(logUrl).json().get('result')
-    for data in arr:
-        tempRequestId = data.get('data')[0:66]
-        if requestId == Web3.to_int(hexstr=tempRequestId):
-            txHash = data.get("transactionHash")
-    link = f"https://mumbai.polygonscan.com/tx/{txHash}#eventlog"
-
-    return {"randomNum": response.json()[0].get("randomWords")[1:-1],
-            "mappingNum": num % 20+1,
-            "transectionLog": link}
-
-
-@app.get("/docs", include_in_schema=False)
-async def get_documentation():
-    return get_swagger_ui_html(
-        openapi_url="/openapi.json",
-        title="API Docs"
-    )
-
-
-@app.get("/openapi.json", include_in_schema=False)
-async def get_open_api_endpoint():
-    return get_openapi(
-        title="API Docs",
-        version="0.0.1",
-        description="This is a test API",
-        routes=app.routes,
-    )
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
+    contract = w3.eth.contract(address=contract_details.get(
+        'address'), abi=contract_details.get('abi'))
+    requests = contract.functions.getRequestStatus(requestId).call()
+    if requests[0]:
+        random_number = requests[1][0]
+        data = {
+            "randomNum": str(random_number),
+            "mappingNum": random_number % 20+1,
+            "transectionLog": "https://mumbai.polygonscan.com/address/0x63855287c54b89b339bb9a715359c7cf307d9c8a"
+        }
+        return data
+    raise HTTPException(
+        status_code=500, detail="Request Not Processed, Kindly try again later")
